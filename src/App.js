975b60a1687d4144dbeea21b5b9857d1c9c05609
Chanin Nusang <realchanin@gmail.com>
2021-11-28 21:34:24 +0700

    currencyChoice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import CurrencyComponent from "./components/CurrencyComponent";
+import money from "./img/money.png";
 
 function App() {
+  const url =
+    "https://v6.exchangerate-api.com/v6/d6e50ec357486dde9f60b2b0/latest/USD";
+  const [currencyChoice, setCurrencyChoice] = useState([]);
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) =>
+        setCurrencyChoice([...Object.keys(data.conversion_rates)])
+      );
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src={money} alt="logo" className="money-img"></img>
+      <h1>เครื่องมือแปลงสกุลเงิน</h1>
+      <div className="container">
+        <CurrencyComponent />
+        <div className="equal"> = </div>
+        <CurrencyComponent />
+      </div>
     </div>
   );
 }

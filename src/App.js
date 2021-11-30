7a4afbf817e2336f751d89db249fccaf75f75166
Chanin Nusang <realchanin@gmail.com>
2021-12-01 06:03:25 +0700

    dynamic-dropdown

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import DropdownComponent from "./components/DropdownComponent";
+import FoodComponent from "./components/FoodComponent";
+import MenuData from "./data/MenuData";
 function App() {
+  const [foods, setFoods] = useState(MenuData);
+  const changeFoodData = (e) => {
+    const category = e.target.value;
+    if (category === "เมนูทั้งหมด") {
+      setFoods(MenuData);
+    } else {
+      const result = MenuData.filter((e) => {
+        return e.menu === category;
+      });
+      setFoods(result);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <DropdownComponent changeFoodData={changeFoodData} />
+      <div className="content">
+        {foods.map((data, index) => {
+          return <FoodComponent key={index} {...data} />;
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/components/FoodComponent.js
@@ -0,0 +1,9 @@
+const FoodComponent = ({ menu, foodName, image_url }) => {
+  return (
+    <div className="single-food">
+      <h2>{foodName}</h2>
+      <img src={image_url} alt={foodName} />
+    </div>
+  );
+};
+export default FoodComponent;
